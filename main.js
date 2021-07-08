95b715c658610ad46f0b5306482d239bee39f7ad
Dan Johnson <danimal.johnson@gmail.com>
2021-07-08 13:23:18 -0700

    Add icons. Add platform checks and features.

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 const { app, BrowserWindow } = require('electron');
 
+// process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.NODE_ENV = 'development';
+const isDev = process.env.NODE_ENV === 'development';
+const isMac = process.platform === 'darwin';
+const isLinux = process.platform === 'linux';
+const isWindows = process.platform === 'win32';
+
 let mainWindow;
 
 function createMainWindow () {
@@ -7,9 +14,25 @@ function createMainWindow () {
     title: 'ImageShrink',
     width: 500,
     height: 600,
+    resizable: isDev,
+    icon: __dirname + '/assets/icons/Icon_256x256.png',
   });
 
-  mainWindow.loadURL('file://' + __dirname + '/app/index.html');
+  // mainWindow.loadURL('file://' + __dirname + '/app/index.html');
+  mainWindow.loadFile(`${__dirname}/app/index.html`);
 }
 
 app.on('ready', createMainWindow);
+
+app.on('window-all-closed', () => {
+  if (!isMac) {
+    app.quit();
+  }
+});
+
+// If this doesn't work on Mac, try this: (BrowserWindow.getAllWindows().length === 0)
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createMainWindow();
+  }
+});
